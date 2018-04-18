654b612a68b2e49e5b7b5576ac61500027a9294e
Luka Locniskar <luka.locniskar@gmail.com>
2018-04-18 16:48:43 +0200

    changes

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 import numpy as np
 
-allData = []
-for line in open('Data/agg_match_stats_0.csv'):
-    newData = line.split()
-    addData = []
-    for entry in newData:
-        addData.append(entry.strip().strip())
-    allData.append(addData)
+allData = np.genfromtxt('Data/agg_match_stats_0.csv')
