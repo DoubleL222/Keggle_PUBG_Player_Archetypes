fa96d303eecd0291ec425fbe82965edce8ff62bc
Luka Locniskar <luka.locniskar@gmail.com>
2018-04-30 12:31:47 +0200

    luka merge

--- PUBG_DataExplorer.py
+++ PUBG_DataExplorer.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #usecols determine which column you are importing
 #nrows=20 will give you the first 20 rows.
-data = pd.read_csv('agg_match_stats_0.csv', usecols=[1,2], error_bad_lines=False)
+data = pd.read_csv('Data/agg_match_stats_0.csv', usecols=[1,2], error_bad_lines=False)
 
 #instead, just append it by the different csv files.
 combinedData = data.append(data)
